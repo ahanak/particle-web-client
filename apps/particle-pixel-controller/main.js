a5d66fdeaf6e0a741e9feace5d5ba0bba2f3237c
André Hanak <andre@doomcom.de>
2016-11-27 23:10:34 +0100

    Added pixelcontroller app

---
+++ apps/particle-pixel-controller/main.js
@@ -0,0 +1,110 @@
+registerApp('particle-pixel-controller');
+
+/** The controller for the particle-pixel-controller app */
+app.controller('PixelCtrl', ['$scope', '$http', '$interval', 'sparkapi', function($scope, $http, $interval, sparkapi) {
+    $scope.device = null;
+    $scope.programNames = null;
+    $scope.programButtons = [];
+    $scope.preset = -1;
+    $scope.numPresets = 0;
+    $scope.turnedOn = "unknown";
+    $scope.fps = null;
+
+    $http.get('apps/particle-pixel-controller/programs.json').then(function(res) {
+        $scope.programNames = res.data;
+    });
+
+    var stopTimer = $interval(function getFps() {
+        if($scope.device != null) {
+            sparkapi.readVariable($scope.device.id, 'fps').then(
+                function (result) {
+                    $scope.fps = result.data.result;
+                },
+                function (error) {
+                }
+            );
+        }
+    }, 10000);
+    $scope.$on('$destroy', function() {
+        if(angular.isDefined(stopTimer)) {
+            // Make sure that the interval is destroyed too
+            $interval.cancel(stopTimer);
+        }
+    });
+
+    /** Turn the device on */
+    $scope.turnOn = function() {
+        sparkapi.callFunction($scope.device.id, 'turnOn').then(
+            function(result) {
+                $scope.turnedOn = (result.data.return_value == 1);
+            },
+            function(error) {}
+        );
+    };
+
+    /** Turn the device off */
+    $scope.turnOff = function() {
+        sparkapi.callFunction($scope.device.id, 'turnOff').then(
+            function(result) {
+                $scope.turnedOn = (result.data.return_value == 1);
+            },
+            function(error) {}
+        );
+    };
+
+    /** Load a specified preset */
+    $scope.loadPreset = function(presetNumber) {
+        sparkapi.callFunction($scope.device.id, 'loadPreset', presetNumber.toString()).then(
+            function(result) {
+                $scope.preset = result.data.return_value;
+            },
+            function(error) {}
+        );
+    };
+
+    /** Load the next preset */
+    $scope.nextPreset = function() {
+        sparkapi.callFunction($scope.device.id, 'nextPreset').then(
+            function(result) {
+                $scope.preset = result.data.return_value;
+            },
+            function(error) {}
+        );
+    };
+
+    $scope.setDevice = function (device) {
+        $scope.device = device;
+        if (device != null) {
+            sparkapi.readVariable(device.id, "numPresets").then(
+                function (result) {
+                    $scope.numPresets = result.data.result;
+                },
+                function (error) {
+                }
+            )
+        }
+    };
+
+    $scope.$watch('numPresets', updateProgramButtons);
+    $scope.$watch('programNames', updateProgramButtons);
+    function updateProgramButtons() {
+        var buttons = [];
+        for(var i = 0; i < $scope.numPresets; i++) {
+            var name = $scope.programNames[i] || "Programm " + i;
+            buttons.push(name);
+        }
+        $scope.programButtons = buttons;
+    }
+
+    /** Reset the $scope. */
+    $scope.clear = function() {
+        $scope.functionResults = {};
+        $scope.functionArgs = {};
+        $scope.variableValues = {};
+        $scope.events = {};
+    };
+
+    //$scope.clear();
+    //$scope.list();
+
+}]);
