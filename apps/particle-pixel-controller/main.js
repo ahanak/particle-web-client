07c1096c6076aa46fabc09f22c4c5a02a1215c72
André Hanak <impressum@a-hanak.de>
2016-11-30 00:20:35 +0100

    Added PIXEL_STATUS event evaluation

--- apps/particle-pixel-controller/main.js
+++ apps/particle-pixel-controller/main.js
@@ -13,8 +13,10 @@ app.controller('PixelCtrl', ['$scope', '$http', '$interval', 'sparkapi', functio
 
     $http.get('apps/particle-pixel-controller/programs.json').then(function(res) {
         $scope.programNames = res.data;
+        updateProgramButtons();
     });
 
+    /** the timer is used to poll the fps variable periodically */
     var stopTimer = $interval(function getFps() {
         if($scope.device != null) {
             sparkapi.readVariable($scope.device.id, 'fps').then(
@@ -73,21 +75,33 @@ app.controller('PixelCtrl', ['$scope', '$http', '$interval', 'sparkapi', functio
         );
     };
 
+    /** Must be called at beginning to define the device to use. */
     $scope.setDevice = function (device) {
         $scope.device = device;
         if (device != null) {
+            sparkapi.registerDeviceEvents($scope.device.id, onEvent);
             sparkapi.readVariable(device.id, "numPresets").then(
                 function (result) {
                     $scope.numPresets = result.data.result;
+                    updateProgramButtons();
+
+                    // ugly: wait for the event connection to be established.
+                    // Unfortunately, we do not have a callback for that.
+                    // We are making a lot of connections at startup, so it might take a while.
+                    // This is why we do it 3s after the numPresets request is finished.
+                    $interval(function(){sparkapi.callFunction(device.id, "reqStatus")}, 3000, 1);
                 },
                 function (error) {
                 }
-            )
+            );
+
+
         }
     };
 
-    $scope.$watch('numPresets', updateProgramButtons);
-    $scope.$watch('programNames', updateProgramButtons);
+    /** The program buttons are generated from programs.json (stored in programNames) and numPresets. */
+    //$scope.$watch('numPresets', updateProgramButtons);
+    //$scope.$watch('programNames', updateProgramButtons);
     function updateProgramButtons() {
         var buttons = [];
         for(var i = 0; i < $scope.numPresets; i++) {
@@ -97,6 +111,16 @@ app.controller('PixelCtrl', ['$scope', '$http', '$interval', 'sparkapi', functio
         $scope.programButtons = buttons;
     }
 
+    /**
+     * The slider movement means that $scope.brightness changes very often.
+     * We do not want to create an http request each time, so we filter that.
+     *
+     * If the brightness slider has been moved, wait 300ms for further movements.
+     * If no further change happens, send the data to the device.
+     *
+     * This is done with a timer. On every slider value change, stop a maybe running timer and start a new timer,
+     * that will transmit the value after 300ms.
+     */
     var brightnessStop = null;
     $scope.$watch('brightness', function(){
         if(brightnessStop != null) {
@@ -111,16 +135,22 @@ app.controller('PixelCtrl', ['$scope', '$http', '$interval', 'sparkapi', functio
         }
     });
 
-
-    /** Reset the $scope. */
-    $scope.clear = function() {
-        $scope.functionResults = {};
-        $scope.functionArgs = {};
-        $scope.variableValues = {};
-        $scope.events = {};
-    };
-
-    //$scope.clear();
-    //$scope.list();
+    /**
+     * Wait for events indicating status changes, so that we can update the GUI.
+     * This is important if this app runs on multiple devices or if the stripe state changes for another reason.
+     */
+    function onEvent(event) {
+        if(event.name == "PIXEL_STATUS") {
+            console.log("Event: " + event.data);
+            try {
+                var data = JSON.parse(event.data);
+                $scope.turnedOn = data.turnedOn;
+                $scope.brightness = data.brightness;
+                $scope.preset = data.program;
+            } catch(error) {
+                console.log(error);
+            }
+        }
+    }
 
 }]);
