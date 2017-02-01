29a0e217a4d85b4bce8e9229468ffbaca6cb0486
André Hanak <andre@doomcom.de>
2017-02-01 01:19:10 +0100

    Added Color Picker

--- apps/particle-pwm-controller/main.js
+++ apps/particle-pwm-controller/main.js
@@ -109,4 +109,63 @@ app.controller('PwmCtrl', ['$scope', '$http', '$interval', 'sparkapi', function(
         }
     }
 
+
+    $scope.sentColor1 = "#0000FF";
+    $scope.color1 = "#0000FF";
+    $scope.sentColor2 = "#00FFFF";
+    $scope.color2 = "#00FFFF";
+    $scope.inlinesettings = {
+        control: 'wheel',
+        inline: true
+    };
+    var colorStop = null;
+    $scope.$watch('color1', watchColor());
+    $scope.$watch('color2', watchColor());
+    function watchColor() {
+        if(colorStop != null) {
+            $interval.cancel(colorStop);
+            colorStop = null;
+        }
+
+        colorStop = $interval(function () {
+            if($scope.color1 != $scope.sentColor1 || $scope.color2 != $scope.sentColor2) {
+                $scope.setColors([gammaCorrect($scope.color), gammaCorrect($scope.color)]);
+                $scope.sentColor1 = $scope.color1;
+                $scope.sentColor2 = $scope.color2;
+            }
+        }, 300, 1);
+    }
+
+
+    function gammaCorrect(color) {
+        var rgb = hexToRgb(color);
+        rgb.r = gammaForComponent(rgb.r);
+        rgb.g = gammaForComponent(rgb.g);
+        rgb.b = gammaForComponent(rgb.b);
+        return rgbToHex(rgb.r, rgb.g, rgb.b);
+    }
+
+    function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    function gammaForComponent(component) {
+        var gammaTable = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25, 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36, 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50, 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89, 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114, 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142, 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175, 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213, 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ];
+        return gammaTable[component];
+    }
+
 }]);
