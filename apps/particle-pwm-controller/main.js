0d0b347fb33bbfbd8892753d555c0ff3d8730579
André Hanak <impressum@a-hanak.de>
2017-02-15 22:45:33 +0100

    Added an update to the color choosers if a scene is changed

--- apps/particle-pwm-controller/main.js
+++ apps/particle-pwm-controller/main.js
@@ -39,7 +39,16 @@ app.controller('PwmCtrl', ['$scope', '$http', '$interval', 'sparkapi', function(
     /** Set the specified colors. */
     $scope.setColors = function(colors_array) {
         sparkapi.callFunction($scope.device.id, 'setColors', colors_array.join(";")).then(
-            function(result) {},
+            function(result) {
+                if(colors_array.length == 2) {
+                    var c1 = '#' + inverseGammaCorrect(colors_array[0]);
+                    var c2 = '#' + inverseGammaCorrect(colors_array[1]);
+                    $scope.sentColor1 = c1;
+                    $scope.color1 = c1;
+                    $scope.sentColor2 = c2;
+                    $scope.color2 = c2;
+                }
+            },
             function(error) {}
         );
     };
@@ -123,7 +132,6 @@ app.controller('PwmCtrl', ['$scope', '$http', '$interval', 'sparkapi', function(
     $scope.$watch('color1', watchColor);
     $scope.$watch('color2', watchColor);
     function watchColor() {
-        console.log("A color changed!");
         if(colorStop != null) {
             $interval.cancel(colorStop);
             colorStop = null;
@@ -148,6 +156,14 @@ app.controller('PwmCtrl', ['$scope', '$http', '$interval', 'sparkapi', function(
         return rgbToHex(rgb.r, rgb.g, rgb.b);
     }
 
+    function inverseGammaCorrect(color) {
+        var rgb = hexToRgb(color);
+        rgb.r = inverseGammaForComponent(rgb.r);
+        rgb.g = inverseGammaForComponent(rgb.g);
+        rgb.b = inverseGammaForComponent(rgb.b);
+        return rgbToHex(rgb.r, rgb.g, rgb.b);
+    }
+
     function hexToRgb(hex) {
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
         return result ? {
@@ -166,9 +182,17 @@ app.controller('PwmCtrl', ['$scope', '$http', '$interval', 'sparkapi', function(
         return componentToHex(r) + componentToHex(g) + componentToHex(b);
     }
 
+    var gammaTable = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25, 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36, 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50, 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89, 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114, 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142, 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175, 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213, 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ];
     function gammaForComponent(component) {
-        var gammaTable = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25, 25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36, 37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50, 51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68, 69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89, 90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114, 115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142, 144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175, 177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213, 215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ];
         return gammaTable[component];
+    }
+    function inverseGammaForComponent(component) {
+        for(var i = 0; i < gammaTable.length; i++) {
+            if(gammaTable[i] >= component) {
+                return i;
+            }
+        }
+        return 255;
     }
 
 }]);
